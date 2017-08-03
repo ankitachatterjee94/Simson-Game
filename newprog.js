a66f6236eb97a43cf01da62fb50f32f3bb6dd039
Ankita Chattrjee <ankita.chatterjee25@gmail.com>
2017-08-04 01:16:14 +0530

    inclusion of else statements

--- newprog.js
+++ newprog.js
@@ -1,218 +1,240 @@
 //declaration of global variables
-var startStop = 0; //stores the switch ON OFF value
-var count = 1; //keeps the count of level
-var blinkDur = 350; //time after which the timeout executes
-var generatedArray = []; //stores random sequence generated
-var userArray = []; //stores user sequence
-var startButton = 0; //stores the start ON value
-var i = -1; //used to traverse the array
-var strictMode = 0; //stores the strict mode ON OFF value
-var show; //used to store interval function
 
-//activates the buttons
-function actButton(){
-    document.getElementById('b1').disabled = false;
-    document.getElementById('b2').disabled = false;
-    document.getElementById('b3').disabled = false;
-    document.getElementById('b4').disabled = false;
-    document.getElementById('b6').disabled = false; 
-}
+var ob={};
+ob.startStop = 0; //stores the switch ON OFF value
+ob.count = 1; //keeps the count of level
+ob.blinkDur = 350; //time after which the timeout executes
+ob.generatedArray = []; //stores random sequence generated
+ob.userArray = []; //stores user sequence
+ob.startButton = 0; //stores the start ON value
+ob.i = -1; //used to traverse the array
+ob.strictMode = 0; //stores the strict mode ON OFF value
+ 
+var game={};
+
+//activates the button
+game.actButton=function() {
+	document.getElementById("b1").disabled = false;
+	document.getElementById("b2").disabled = false;
+	document.getElementById("b3").disabled = false;
+	document.getElementById("b4").disabled = false;
+	document.getElementById("b6").disabled = false;
+};
 
 //deactivates the buttons
-function deactButton(){
-    document.getElementById('b1').disabled = true;
-    document.getElementById('b2').disabled = true;
-    document.getElementById('b3').disabled = true;
-    document.getElementById('b4').disabled = true;
-    document.getElementById('b6').disabled = true;
-}
+game.deactButton=function() {
+	document.getElementById("b1").disabled = true;
+	document.getElementById("b2").disabled = true;
+	document.getElementById("b3").disabled = true;
+	document.getElementById("b4").disabled = true;
+	document.getElementById("b6").disabled = true;
+};
 
 //checks whether the switch is ON or OFF
-function switch1(){
-    clearInterval(show);
-    startStop = !startStop; //works as a NOT gate
-    if(startStop)
-        {
-            document.getElementById('toggle').style.left = '1.2vw'; //switch shifts to ON 
-            document.getElementById('cNumber').innerHTML = '--';           
-        }
-    else
-        {
-            document.getElementById("blinkT").style.background="black"; //switch shifts to OFF
-            reset();
-        }
-}
+game.switch1=function() {
+	
+	ob.startStop = !ob.startStop; //works as a NOT gate
+	if (ob.startStop) {
+		document.getElementById("toggle").style.left = "50%"; //switch shifts to ON 
+		document.getElementById("cNumber").innerHTML = "--";
+		
+	} 
+	else {
+		document.getElementById("blinkT").style.background = "black"; //switch shifts to OFF
+		game.reset();
+		
+	}
+};
 
 //resets the global variables to its initial value
-function reset(){
-    startStop = 0;
-    count = 1;
-    generatedArray = [];
-    userArray = [];
-    startButton = 0;
-    i = -1;
-    clearInterval(show);
-    document.getElementById('cNumber').innerHTML = '';
-    document.getElementById('toggle').style.left = '0px';
-}
+game.reset=function() {
+	ob.startStop = 0;
+	ob.count = 1;
+	ob.generatedArray = [];
+	ob.userArray = [];
+	ob.startButton = 0;
+	ob.i = -1;
+	document.getElementById("cNumber").innerHTML = "";
+	document.getElementById("toggle").style.left = "0px";
+};
 
 //starting the game on start button click
-function start1(){
-    clearInterval(show);
-    if(startStop)
-        {
-            startButton = 1;
-            count = 1;
-            generatedArray = [];
-            userArray = [];
-            i = -1;
-            document.getElementById('cNumber').innerHTML = '--';
-            setTimeout(function(){
-                    nextMove();
-                    document.getElementById('cNumber').innerHTML = '0'+count;
-                  },400);
-            setTimeout(function(){
-                    actButton();
-                  },500);
-        }
-        
-}
+game.start1=function() {
+		
+	if (ob.startStop) {
+		ob.startButton = 1;
+		ob.count = 1;
+		ob.generatedArray = [];
+		ob.userArray = [];
+		ob.i = -1;
+		document.getElementById("cNumber").innerHTML = "--";
+		setTimeout(function() {
+			game.nextMove();
+			document.getElementById("cNumber").innerHTML = "0" + ob.count;
+		}, 400);
+		setTimeout(function() {
+			game.actButton();
+		}, 500);
+		
+	}
+	else
+		ob.i = -1;
+
+};
 
 //generation of new pattern
-function nextMove(){
-    var val = randomInt(1,4);
-    generatedArray.push(val);
-    blink(val);
-}
+game.nextMove=function() {
+	var val = game.randomInt(1, 4);
+	ob.generatedArray.push(val);
+	game.blink(val);
+};
 
 //generation of a random integer
-function randomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+game.randomInt=function(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
 //blinking of the buttons based on the sequence
-function blink(number){
-    switch(number){
-        case 1:document.getElementById('b1').style.opacity = 0.5;
-               setTimeout(function(){
-                   document.getElementById('b1').style.opacity = 1;
-                   document.getElementById("b1sound").play();
-                 },blinkDur);
-                   break;
-        case 2:document.getElementById('b2').style.opacity = 0.5;
-               setTimeout(function(){
-                   document.getElementById('b2').style.opacity = 1;
-                   document.getElementById("b2sound").play();
-                 },blinkDur);
-                   break;
-        case 3:document.getElementById('b3').style.opacity = 0.5;
-               setTimeout(function(){
-                   document.getElementById('b3').style.opacity = 1;
-                   document.getElementById("b3sound").play();
-                 },blinkDur);
-                   break;
-        case 4:document.getElementById('b4').style.opacity = 0.5;
-               setTimeout(function(){
-                   document.getElementById('b4').style.opacity = 1;
-                   document.getElementById("b4sound").play();
-                 },blinkDur);
-                   break;
-    }
-}
+game.blink=function(number) {
+	switch (number) {
+	case 1:
+		document.getElementById("b1").style.opacity = 0.5;
+		setTimeout(function() {
+			document.getElementById("b1").style.opacity = 1;
+			document.getElementById("b1sound").play();
+		}, ob.blinkDur);
+		break;
+	case 2:
+		document.getElementById("b2").style.opacity = 0.5;
+		setTimeout(function() {
+			document.getElementById("b2").style.opacity = 1;
+			document.getElementById("b2sound").play();
+		}, ob.blinkDur);
+		break;
+	case 3:
+		document.getElementById("b3").style.opacity = 0.5;
+		setTimeout(function() {
+			document.getElementById("b3").style.opacity = 1;
+			document.getElementById("b3sound").play();
+		}, ob.blinkDur);
+		break;
+	case 4:
+		document.getElementById("b4").style.opacity = 0.5;
+		setTimeout(function() {
+			document.getElementById("b4").style.opacity = 1;
+			document.getElementById("b4sound").play();
+		}, ob.blinkDur);
+		break;
+	}
+};
 
 //checks the user button
-function buttonClick(btn){
-    if(startButton){
-        userArray.push(btn);
-        blink(btn);
-        checklogic();
-    }
-}
+game.buttonClick=function(btn) {
+	
+	if (ob.startButton) {
+		ob.userArray.push(btn);
+		game.blink(btn);
+		game.checklogic();
+	}
+	else
+		ob.i=-1;
+	
+};
 
 //checks whether the strict mode in ON
-function strict1(){
-    strictMode = !strictMode;
-    if(startStop)
-        {
-            strictMode?document.getElementById('blinkT').style.background="red":document.getElementById('blinkT').style.background="black";
-        }    
-}
+game.strict1=function() {
+	ob.strictMode = !ob.strictMode;
+	
+	if (ob.startStop) {
+		if(ob.strictMode) {
+			document.getElementById("blinkT").style.background = "red";
+			
+		}
+		else {
+			document.getElementById("blinkT").style.background = "black";
+			
+		}
+	}
+	else
+		ob.i=-1;
+	
+};
 
 //matching of user sequence with the generated sequence
-function checklogic(){
-    i++;
-    if(count==20)
-            {
-                alert("You Won!");
-                setTimeout(function(){
-                    start1();
-                  },blinkDur+50);
-            }
-    else if(count<20)
-        {
-            if(generatedArray[i]!=userArray[i])
-                {
-                    deactButton(); 
-                    if(strictMode)
-                        {
-                            document.getElementById('cNumber').innerHTML = '';
-                            setTimeout(function(){
-                                document.getElementById('cNumber').innerHTML = '!!';
-                                document.getElementById('Wrong').play();
-                              },500);
-                            setTimeout(function(){
-                                start1();
-                            },800);
-                            return;
-                        }
-                    else
-                        {
-                            document.getElementById('cNumber').innerHTML = '';
-                            setTimeout(function(){
-                                document.getElementById('cNumber').innerHTML = '!!';
-                                document.getElementById('Wrong').play();
-                              },700);
-                            setTimeout(function(){
-                                document.getElementById('cNumber').innerHTML = '0'+count;
-                              },1200);
-                            userArray=[];
-                            i=-1;
-                            setTimeout(function(){
-                                traverse();
-                              },1500);
-                            setTimeout(function(){
-                                actButton();
-                              },blinkDur+generatedArray.length*1600);         
-                        }
-                }
-            if(i>=generatedArray.length-1)
-            {
-                count++;
-                setTimeout(function(){
-                    document.getElementById('cNumber').innerHTML = '0'+count;
-                  },1000);
-                i = -1;
-                userArray = [];
-                traverse();
-                setTimeout(function(){
-                    nextMove();
-                  },blinkDur+generatedArray.length*1000); 
-                setTimeout(function(){
-                    actButton();
-                  },blinkDur+generatedArray.length*950);         
-            } 
-        }                    
-    
-}
+game.checklogic=function() {
+	
+	ob.i++;
+	if (ob.count < 20) {
+		if (ob.generatedArray[ob.i] != ob.userArray[ob.i]) {
+			game.deactButton();
+			
+			if (ob.strictMode) {
+				document.getElementById("cNumber").innerHTML = "";
+				setTimeout(function() {
+					document.getElementById("cNumber").innerHTML = "!!";
+					document.getElementById("Wrong").play();
+				}, 500);
+				setTimeout(function() {
+					game.start1();
+				}, 800);
+				return;
+			} else {
+				
+				document.getElementById("cNumber").innerHTML = "";
+				setTimeout(function() {
+					document.getElementById("cNumber").innerHTML = "!!";
+					document.getElementById("Wrong").play();
+				}, 700);
+				setTimeout(function() {
+					document.getElementById("cNumber").innerHTML = "0" + ob.count;
+				}, 1200);
+				ob.userArray = [];
+				ob.i = -1;
+				setTimeout(function() {
+					game.traverse();
+				}, 1500);
+				setTimeout(function() {
+					
+					game.actButton();
+				}, 5000);
+			}
+		}
+		if (ob.i >= ob.generatedArray.length - 1) {
+			ob.count++;
+			setTimeout(function() {
+				document.getElementById("cNumber").innerHTML = "0" + ob.count;
+			}, 1000);
+			ob.i = -1;
+			ob.userArray = [];
+			game.traverse();
+			setTimeout(function() {
+				game.nextMove();
+			}, ob.blinkDur + ob.generatedArray.length * 1000);
+			setTimeout(function() {
+				game.actButton();
+			}, ob.blinkDur + ob.generatedArray.length * 950);
+		}
+	}
+	else if (ob.count == 20) {
+		document.getElementById("cNumber").innerHTML = "WON";
+		setTimeout(function() {
+			game.start1();
+		}, ob.blinkDur + 50);
+	}
+	
+	else
+		return false;
+	
+};
 
 //traversing through the generated sequence
-function traverse(){
-    deactButton();
-    var j=0;
-    show = setInterval(function(){
-        blink(generatedArray[j]);
-        j++;
-        if(j>=generatedArray.length)
-            clearInterval(show);
-    },blinkDur+400);
-}
+game.traverse=function() {
+	game.deactButton();
+	var j = 0;
+	var show = setInterval(function() {
+		game.blink(ob.generatedArray[j]);
+		j++;
+		if (j >= ob.generatedArray.length)
+			clearInterval(show);
+	}, ob.blinkDur + 400);
+};
